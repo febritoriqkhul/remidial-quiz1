0ecee44a18efcfa953d6ec5937ef0baf5911ba63
Febri TK <febritoriqkhul@gmail.com>
2018-11-04 13:16:55 +0700

    Pembuatan class Cpu.java

--- src/Cpu.java
+++ src/Cpu.java
@@ -9,5 +9,32 @@
  * @author Febri TK
  */
 public class Cpu {
+    private String namaCPU;
+    private int hargaCPU;
     
+    public Cpu() {
+    }
+
+    public String getNamaCPU() {
+        return namaCPU;
+    }
+
+    public void setNamaCPU(String namaCPU) {
+        this.namaCPU = namaCPU;
+    }
+
+    public int getHargaCPU() {
+        return hargaCPU;
+    }
+
+    public void setHargaCPU(int hargaCPU) {
+        this.hargaCPU = hargaCPU;
+    }
+    
+    public String info(){
+        String info="";
+        info += "Nama CPU: "+this.namaCPU+"\n";
+        info += "Harga CPU: "+this.hargaCPU+"\n";
+        return info;
+    }
 }
